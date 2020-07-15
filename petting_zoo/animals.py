f28909bac2c3741f5c521099ee8d52d2e0df0b28
Adrian Garmendia <dejima@gmail.com>
2020-07-15 15:47:25 -0500

    15 classes later

---
+++ petting_zoo/animals.py
@@ -0,0 +1,187 @@
+# import the python datetime module to help us create a timestamp
+from datetime import date
+
+class Llama:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+Bartholomew = Llama("Bartholomew", "3 legged Peruvian")
+        
+class Pig:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+Lassie = Pig("Lassie", "Tibetian Straight Tailed")
+
+class Porcupine:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+Fluffy = Porcupine("Fluffy", "Curmudgeon")
+
+class Armadillo:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+Tank = Armadillo("Tank", "Southern Yard Destroyer")
+
+class Groundhog:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+Beaudreaux = Groundhog("Beaudreaux", "Louisiana Drunken Groundhog")
+
+class Lawyers:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+Toby = Lawyers("Toby", "The Worst")
+
+class Viper:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+Bitey = Viper("Bitey", "Pit")
+
+
+class Lobbyist:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+Karen = Lobbyist("Karen", "Antivaxxer")
+
+class Python:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+Kaa = Python("Kaa", "Grey Cartoon")
+
+class Cobra:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+Dipper = Cobra("Dipper", "Spitting")
+
+class Gator:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+Wally = Gator("Wally", "Hanna Barbera")
+
+class Gar:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+Cuddles = Gar("Cuddles", "Eastern Brim Killer")
+
+class Beaver:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+Pickles = Beaver("Pickles", "Angry")
+
+class Shark:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+JabberJaws = Shark("Jabber Jaws", "Great White")
+
+class Godzilla:
+
+    def __init__(self, name, species, ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+Mechazilla = Godzilla("Mechazilla", "Robot")
+
+
+
+
